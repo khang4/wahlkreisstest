3172e6c05368dafaa38c8bc4b22105d9499fac3e
risonakamo <hmykanae@gmail.com>
2017-10-25 17:29:14 -0400

    red menu thing

---
+++ mapcontrol.js
@@ -0,0 +1,63 @@
+class _mapControl
+{
+    constructor()
+    {
+        this.map=new google.maps.Map(document.querySelector(".map"),{
+            center:{lat:38.682,lng:-77.344},
+            zoom:8
+        });
+
+        this.direction=new google.maps.DirectionsService();
+
+        this.menu=document.querySelector(".menu-bar");
+
+        this.menuSet();
+    }
+
+    menuSet()
+    {
+        document.querySelector(".minimise").addEventListener("click",(e)=>{
+            this.menu.classList.add("hidden");
+        });
+    }
+
+    //draw a polyline from a coordinate to another, following
+    //roads by using the direction api
+    roadLineDrawtest()
+    {
+        this.direction.route({
+            origin:{lng:-78,lat:39.137},
+            destination:{lat:39.281,lng:-76.60},
+            travelMode:google.maps.TravelMode.DRIVING
+        },(r,s)=>{
+            var pathPoints=r.routes[0].overview_path;
+            var path=new google.maps.Polyline({
+                path:pathPoints
+            });
+
+            path.setMap(this.map);
+        });
+    }
+
+    //load geojson from a file and alternate colouring
+    //between red and blue
+    loadGeoJsonTest()
+    {
+        this.map.data.loadGeoJson("md-district.geojson");
+
+        var mapcolorCount=1;
+
+        this.map.data.setStyle((f)=>{
+            var color="red";
+
+            if (mapcolorCount%2)
+            {
+                color="blue";
+            }
+
+            mapcolorCount++;
+
+            return {fillColor:color,strokeColor:color,strokeWeight:1};
+        });
+    }
+}
---
+++ mapcontrol.js.h
@@ -0,0 +1,12 @@
+class _mapControl
+{
+    Map map; //map object from google map api
+    DirectionService direction; //direction service object
+
+    element menu;
+
+    void menuSet();
+
+    void roadLineDrawtest();
+    void loadGeoJsonTest();
+}
