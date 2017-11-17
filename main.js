38c58a6b38bcdaab514dde7eb10f06a69c373475
risonakamo <hmykanae@gmail.com>
2017-11-17 15:29:07 -0500

    backend xhr test

--- main.js
+++ main.js
@@ -6,3 +6,20 @@ function main()
 {
     control=new _mapControl;
 }
+
+function htest()
+{
+    console.log("he");
+    var r=new XMLHttpRequest();
+
+    r.open("GET","https://districtlands.herokuapp.com/");
+
+    r.onreadystatechange=()=>{
+        if (r.readyState==4)
+        {
+            console.log(r.response);
+        }
+    };
+
+    r.send();
+}
